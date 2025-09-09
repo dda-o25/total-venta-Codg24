112711e76745c7ae2aada652d7554fee46f02d79
Codg24 <carlos.diaz@iteso.mx>
2025-09-09 08:27:59 -0600

    Primera propuesta de solución

--- total-venta.py
+++ total-venta.py
@@ -1,15 +1,20 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Determinar total de venta
+Si la venta es mayor a 1000, aplicar 5% de descuento
 """
 
 # Declaraciones
-CONSTANTE = valor
+MINI_VENTA_P_DESC = 1000
+DESCUENTO = 0.05
 
 # Entradas
-entrada = input()
+cantidad = float(input("¿Cuántos artículos? "))
+precio = float(input("Precio unitario: "))
 
 # Proceso
-variable = expresion
+total = cantidad * precio
+if total > MINI_VENTA_P_DESC:
+    total = total * (1 - DESCUENTO)
 
 # Salidas
-print(salida)
+print("El total es", total)
